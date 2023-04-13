a8bad1a6736aabe2e64599ca239d7df3c0365c05
Stanislav <129663783+Stanislav82-hub@users.noreply.github.com>
2023-04-13 08:13:57 +0300

    Update Program.cs

--- Task_8_All_even_from_numbet_till_N/Program.cs
+++ Task_8_All_even_from_numbet_till_N/Program.cs
@@ -1,9 +1,6 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает
 // все чётные числа от 1 до N.
-//Console.WriteLine("Please fill in any number:");
-//int number = int.Parse(Console.ReadLine());
 
-//Console.WriteLine("Hello, World!");
 Console.WriteLine("Введите число N");
 int n = int.Parse(Console.ReadLine());
 for(int i=1;i<=n;i++)
