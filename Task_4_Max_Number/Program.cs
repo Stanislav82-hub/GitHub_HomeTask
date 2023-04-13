271fe847d3f423cae739bcced03b0f699918058a
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-13 03:35:29 +0300

    Task_4_Max_Number: Fill in 2 numbers and show the greatest

---
+++ Task_4_Max_Number/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Please fill in number1");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Please fill in number2");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine($"{number1} is greater than {number2}");
+}
+else
+{
+    Console.WriteLine($"Number {number2} is greater than {number1}");
+}
