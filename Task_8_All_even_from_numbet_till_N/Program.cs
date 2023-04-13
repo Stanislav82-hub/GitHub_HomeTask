346f825f54251b44572da4867b9b28275bd631e6
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-13 09:26:17 +0300

    Commit to eliminate not commited files

--- Task_8_All_even_from_numbet_till_N/Program.cs
+++ Task_8_All_even_from_numbet_till_N/Program.cs
@@ -1,10 +1,7 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает
 // все чётные числа от 1 до N.
-//Console.WriteLine("Please fill in any number:");
-//int number = int.Parse(Console.ReadLine());
 
-//Console.WriteLine("Hello, World!");
-Console.WriteLine("Введите число N");
+Console.WriteLine("Please fill in number N");
 int n = int.Parse(Console.ReadLine());
 for(int i=1;i<=n;i++)
 {
