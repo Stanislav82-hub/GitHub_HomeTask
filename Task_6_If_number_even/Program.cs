88e20e63a7bd542fc1d63a07bfb7028716dd7f6b
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-13 05:35:49 +0300

    Task_6_If_number_even

---
+++ Task_6_If_number_even/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Please fill in any number:");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 ==0)
+{
+    Console.WriteLine("This number is even");
+}
+else
+{
+    Console.WriteLine("This number is not even");
+}
