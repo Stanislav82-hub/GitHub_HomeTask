deaf32008260a586ce56dcf5655496390969d9e2
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-15 13:55:59 +0300

    Task_13_3rd_digit_of_3

---
+++ Task_13_3rd_digit_of_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Please fill in three-digit number:");
+int number = int.Parse(Console.ReadLine());
+int a = number % 10;
+if(number < 100)
+{
+    Console.WriteLine($"There is no 3rd digit in this number: {number}");
+}
+else 
+{
+    if(number >= 1000)
+    {
+       Console.WriteLine("Please fill in three-digit number:");
+    }
+    else
+    {
+        Console.WriteLine($"The 3rd digit of this number is {a}");
+    }
+        
+}
