464eb16c409c6d9376efb04c8ca401c1661266af
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-15 08:38:26 +0300

    Task_10_2nd_number_of_3

---
+++ Task_10_2nd_number_of_3/Program.cs
@@ -0,0 +1,7 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Please fill in three-digit number:");
+int number = int.Parse(Console.ReadLine());
+int a = number / 10;
+Console.WriteLine(a);
+int result = a % 10;
+Console.WriteLine($"Second digit is {result}");
