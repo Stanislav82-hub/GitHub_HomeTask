8f01e6e5f74dc53564388738ad5a6350c131ac25
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-16 02:54:37 +0300

    Task_15_Is_it_a_weekend

---
+++ Task_15_Is_it_weekend/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели
+// и проверяет, является ли этот день выходным
+Console.WriteLine("Please fill in week's day number:");
+int number = int.Parse(Console.ReadLine());
+if(number < 6) 
+{
+    Console.WriteLine("It is not a weekend");
+}
+else
+{
+    Console.WriteLine("It is a weekend");
+}
