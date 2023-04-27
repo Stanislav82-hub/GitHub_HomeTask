d71d0fe9db5289620c3c8a844abfa9f098cdc642
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-27 13:46:22 +0300

    Seminar_4_Task_29

---
+++ Seminar_4_Task_29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] RandomArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+        array[i] = (new Random().Next(0, 100));
+    return array;
+}
+Console.Write("Fill in number \t");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Your random array is: "+String.Join(", ", RandomArray(number)));
