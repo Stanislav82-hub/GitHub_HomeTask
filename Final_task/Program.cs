8312a713972c35e975b7760ab3e4d9765e42189a
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-09-08 17:09:49 +0300

    Method SecondArrayLength is created

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -8,7 +8,7 @@ string[] CreateAnitialArray()
     Console.Write("How many elements AnitialArray should consist of?:\t");
     int AnitialArrayLenght = int.Parse(Console.ReadLine());
     Console.WriteLine();
-    
+
     string[] AnitialArray = new string[AnitialArrayLenght];
     for (int i = 0; i < AnitialArray.Length; i++)
     {
@@ -26,4 +26,47 @@ string[] CreateAnitialArray()
     Console.WriteLine("========================================================================");
     return AnitialArray;
 }
-CreateAnitialArray();
+
+void SecondArrayLength(string[] AnitialArray)
+{
+    int n = 0;
+    for (int i = 0; i < AnitialArray.Length; i++)
+    {
+        if (AnitialArray[i].Length <= 3)
+            n++;
+    }
+
+    if (n < 1)
+    {
+        Console.WriteLine("There are no elements in AnitialArray to include in FinalArray");
+
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Length of FinalArray is {n} elements and they are following:");
+
+        string[] FinalArray = new string[n];
+
+
+        int j = 0;
+        for (int i = 0; i < AnitialArray.Length; i++)
+        {
+            if (AnitialArray[i].Length <= 3)
+            {
+                FinalArray[j] = AnitialArray[i];
+                j++;
+            }
+        }
+
+        for (int i = 0; i < FinalArray.Length; i++)
+        {
+            Console.Write($" {FinalArray[i]} |");
+        }
+        System.Console.WriteLine();
+        System.Console.WriteLine();
+    }
+    
+}
+
+SecondArrayLength(CreateAnitialArray());
